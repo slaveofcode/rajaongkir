4690613627302f0dff1f8d0be6459d7e871cede8
Aditya Kresna Permana <slaveofcode@gmail.com>
2015-12-09 18:03:55 +0700

    Added install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 setup(
   name='rajaongkir',
   packages=['rajaongkir'],  # must be filled with your python directories
-  version='1.1.1',
+  version='1.1.2',
   description='Simple Python module to grab api data from rajaongkir.com',
   author='Aditya Kresna Permana',
   author_email='zeandcode@gmail.com',
   url='https://github.com/slaveofcode/rajaongkir',  # use the URL to the github repo
-  download_url='https://github.com/slaveofcode/rajaongkir/archive/v1.1.1.zip',
+  download_url='https://github.com/slaveofcode/rajaongkir/archive/v1.1.2.zip',
+  install_requires=[
+    'requests'
+  ],
   keywords=['ongkir', 'rajaongkir', 'JNE', 'TIKI', 'Pos'],  # arbitrary keywords
   classifiers=[],
 )
